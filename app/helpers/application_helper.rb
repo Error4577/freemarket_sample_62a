bbc4eb64972cbe4d16257f3fdca8c41c5e5832ed
Error4577 <donttreadonme4577@gmail.com>
2019-12-11 21:57:13 +0900

    遅延読み込みを導入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,6 @@ module ApplicationHelper
     else
       options[:class] = "lazyload #{options[:class]}"
     end
-    image_tag("画像を読み込んでない時の軽い画像", options) + ("<noscript>#{image_tag(source, options)}</noscript>").html_safe
+    image_tag("ajax-loader.gif", options) + ("<noscript>#{image_tag(source, options)}</noscript>").html_safe
   end
 end
