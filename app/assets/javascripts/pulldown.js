6f4d2d6bfdf00eb7273af757f2c2f19b1f49c807
kktakano <kktakano03@gmail.com>
2019-12-16 15:19:33 +0900

    merging_to_fork_source
    ファイルを指定してフォークもとへマージ

--- app/assets/javascripts/pulldown.js
+++ app/assets/javascripts/pulldown.js
@@ -1,18 +1,4 @@
 $(function(){
-  // childrenカテゴリのhtml作成
-  // function buildCategoryList_Children(category_children, selected_category_root){
-  //   category_children.forEach(function(children) {
-  //     $(`#${selected_category_root}`).append(buildCategoryBox_children(children));
-  //   });
-
-  //   function buildCategoryBox_children(children){
-  //     var optionHtmlChild = `<li class="header__menu-box--left__children__child" value="${children.id}">
-  //                              <a href="#">${children.name}</a>
-  //                              <ul class="header__menu-box--left__grand-children" id="${children.id}"></ul>
-  //                            </li>`
-  //     return optionHtmlChild;
-  //   };
-  // };
   // grandchildrenカテゴリのhtml作成
   function buildCategoryList_GrandChildren(category_grandchildren, selected_category_children){
     category_grandchildren.forEach(function(grandchildren) {
@@ -27,23 +13,6 @@ $(function(){
     };
   };
 
-  // rootカテゴリにmouseenterで発火
-  // $(document).on("mouseenter",".header__menu-box--left__parents__parent", function(e) {
-  //   var selected_category_root = $(this).val();
-  //   $.ajax({
-  //     url: "/products/get_category_children",
-  //     type: "GET",
-  //     data: {root_category_id: selected_category_root},
-  //     dataType: "json"
-  //   })
-  //   .done(function(category_children){
-  //     buildCategoryList_Children(category_children, selected_category_root);
-  //   })
-  //   .fail(function(){
-  //     console.log('error');
-  //   })
-
-  // });
   // childrenカテゴリにmouseenterで発火
   $(document).on("mouseenter",".header__menu-box--left__children__child", function(e) {
     var selected_category_children = $(this).val();
